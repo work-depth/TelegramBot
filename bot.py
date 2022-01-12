723516f62280c8fc8ce802f1edb179b2906320d2
Naman Kaushik <kaushiknaman.dk@gmail.com>
2022-01-12 16:48:43 +0530

    link button placeholders

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 from telegram.ext import *
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 import os
 
 TOKEN = "5070020632:AAF8FSn_WUBQBLRdAuSXmjqjjC_y3MjfCf8"
@@ -11,6 +12,13 @@ def start(update, context):
     # print(update)
     msg  = "Hello, I am bhan_chod."
     update.message.reply_text(msg)
+    update.message.reply_text(
+    'Cute pics',
+    reply_markup=InlineKeyboardMarkup([
+        [InlineKeyboardButton(text='on Facebook', url='https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=67773a9d419786091c958b2ad08eae5e')],
+        [InlineKeyboardButton(text='on Telegram', url='https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=67773a9d419786091c958b2ad08eae5e')],
+    ])
+)
 
 def main():
     """Start the bot."""
