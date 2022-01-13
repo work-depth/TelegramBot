0446280fbe48c3c94cb3b7888ddd1b826c5f7c4d
Yashwardhan Chaudhuri <yashwardhan20417@iiitd.ac.in>
2022-01-13 23:43:39 +0530

    mongoDB for mongoDB work

--- mongoDB.py
+++ mongoDB.py
@@ -1,5 +1,34 @@
+from backend import *
 import pymongo
 from pymongo import Mongoclient
 
-cluster = Mongoclient(mongodb+srv://yashwardhan:PY74NORNY5OnUrH6@cluster0.1wicn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority)
-db = cluster["rooms"]
+cluster = Mongoclient("mongodb+srv://yashwardhan:PY74NORNY5OnUrH6@cluster0.1wicn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["database"]
+collection = db["rooms"]
+
+def createUser(userID, name, profile, isAdmin):
+    user = User(userID, name, profile, isAdmin)
+
+
+def createRoom(groupID, userList, adminList):
+    try:
+        insertion = {"_id" : groupID}
+        userDict = {}
+        adminDict = {}
+        for user in userList:
+            userDict[user.name] = user
+        for admin in adminList:
+            userDict[user.name] = user
+        bibliography = {}
+        notifyMessages = {}
+        insertion["userList"] = userDict
+        insertion["adminDict"] = adminDict
+        insertion["bibliography"] = bibliography
+        insertion["notifyMessages"] = notifyMessages
+        collection.insert_one(insertion)
+    except Exception as e:
+        print(e)
+        
+
+    
+
