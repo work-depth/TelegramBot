ca917cdbbaf432b8fce9608f1d87f458fbf0715e
mishraanis <mishraanis@gmail.com>
2022-01-13 19:45:43 +0530

    commit 2

---
+++ backend.py
@@ -0,0 +1,25 @@
+class User:
+    def __init__(self, ID, name, profile, isAdmin) -> None:
+        self.ID = ID
+        self.name = name
+        self.profile = profile
+        self.isAdmin = isAdmin
+
+
+def catch(taskID):
+
+def update_task([ID, attr, “message”]):
+
+def task_list(attr):
+
+def update_bibliography(attr, link):
+
+def tasks(attr1, attr2):
+
+def member(attr1, telegramID):
+
+def admin(attr1, telegramID):
+
+def reminder(attr1, message):
+    
+def notify(message):
