4941501e2e1b5e9694d09ae9fca6076ed7f8aed5
Naman Kaushik <kaushiknaman.dk@gmail.com>
2022-01-19 18:20:32 +0530

    add user parameters

--- bot.py
+++ bot.py
@@ -1,12 +1,21 @@
 import logging
+from turtle import back
 from telegram.ext import *
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 import os
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+# import mongoDB
 
 logger = logging.getLogger(__name__)
 
 TOKEN = "5070020632:AAF8FSn_WUBQBLRdAuSXmjqjjC_y3MjfCf8"
+allMembers = []
+
+# register command for each user. call addUser
+# admin command to add or remove admins
+# 
+
+roles = []
 print(TOKEN)
 # logging.basicConfig(filename="newfile.log",
 #                     format='%(asctime)s %(message)s',
@@ -22,6 +31,31 @@ def start(update, context):
         [InlineKeyboardButton(text='on Telegram', url='https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=67773a9d419786091c958b2ad08eae5e')],
     ])
 )
+
+# Helper function to check is a person is an admin or not.
+def get_admin_ids(bot, chat_id):
+    """Returns a list of admin IDs for a given chat. Results are cached for 1 hour."""
+    return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
+
+# Command handler for registering a user.
+def registerUser(update, context):
+    # print(update)
+    temp = update.message.text.split()
+    if(len(temp)<2):
+        update.message.reply_text("Oops! You forgot to mention the role. Try again.")
+    else:
+        currUser = update.message.from_user
+        currId = currUser.id
+        currName = currUser.first_name+" "+currUser.last_name
+        currIsAdmin = currId in get_admin_ids(updater.bot, update.message.chat.id)
+        currGrpId = update.message.chat.id
+        print(update)
+        print()
+        print(currId, currName, temp[1], currIsAdmin, currGrpId)
+        print()
+        # mongoDB.addUser(currId, currName, temp[1], currIsAdmin, currGrpId)
+
+      
 
 def addMemberIfNotAdded(update, context):
     if(update.message.from_user not in allMembers):
@@ -43,11 +77,11 @@ def echo(update, context):
 def new_member(update, context):
     for member in update.message.new_chat_members:
         if member.username == 'bhanChod_bot':
-            update.message.reply_text('Thanks for adding me.\nHope I will be able to help as much as possible. Please say Hi to me once so I can get to know all of you.')
-            global allMembers
-            allMembers = updater.bot.get_chat_administrators(update.message.chat.id)
-            for member in allMembers:
-                print(member.user.first_name)
+            update.message.reply_text('Thanks for adding me.\nHope I will be able to help as much as possible. Please let me know you roles so I can enter it in my system.\nDear admin, please send me a list of all roles availbles')
+            # global allMembers
+            # allMembers = updater.bot.get_chat_administrators(update.message.chat.id)
+            # for member in allMembers:
+            #     print(member.user.first_name)
             
 
         elif member.username != 'YourBot':
@@ -74,13 +108,10 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("register", registerUser))
     dp.add_handler(MessageHandler(Filters.text, echo))
-
     dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
-
     dp.add_error_handler(error)
-
-
     # Start the Bot
     updater.start_polling()
     updater.idle()
